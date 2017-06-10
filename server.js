a0a7976b60289f0cf2ccf49fc2458c912f732a36
shdb1 <shadab2266@gmail.com>
2017-06-10 18:12:32 +0530

    Create server.js
    added db connectivity

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+var db
 app.use(bodyParser.urlencoded({extended: true}))
 app.listen(3000, function() {
 	  console.log('listening on 3000')
@@ -11,12 +12,55 @@ app.get('/', function(req, res) {
  // res.send('Hello World')
 })
 
+
+/*var mongodb = require('mongodb');
+var uri = 'mongodb://shadab:shadab2266@ds119772.mlab.com:19772/shdb';
+mongodb.MongoClient.connect(uri, function (err, db) {
+     adventure! 
+});*/
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+var insertDocument = function(db, callback) {
+	   db.collection('restaurants').insertOne( {
+	      "address" : {
+	         "street" : "2 Avenue",
+	         "zipcode" : "10075",
+	         "building" : "1480",
+	         "coord" : [ -73.9557413, 40.7720266 ]
+	      },
+	      "borough" : "Manhattan",
+	      "cuisine" : "Italian",
+	      "grades" : [
+	         {
+	            "date" : new Date("2014-10-01T00:00:00Z"),
+	            "grade" : "A",
+	            "score" : 11
+	         },
+	         {
+	            "date" : new Date("2014-01-16T00:00:00Z"),
+	            "grade" : "B",
+	            "score" : 17
+	         }
+	      ],
+	      "name" : "Vella",
+	      "restaurant_id" : "41704620"
+	   }, function(err, result) {
+	    assert.equal(err, null);
+	    console.log("Inserted a document into the restaurants collection.");
+	    callback();
+	  });
+	};
+	
 app.post('/quotes', function(req, res) {
   console.log('Hellooooooooooooooooo!')
-    console.log(req.body)
-  res.sendFile(__dirname + '/index.html')
+  MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+	  insertDocument(db, function() {
+	      db.close();
+	  });
+	});
 })
 
-/*MongoClient.connect('link-to-mongodb',function (err, database) 	{
-  // ... start the server
-})*/
